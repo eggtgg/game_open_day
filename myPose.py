3a52b1446fea824e57cd337255e807e348f5cb36
eggtgg <73218635+eggtgg@users.noreply.github.com>
2025-03-21 22:43:22 +0700

    update

--- myPose.py
+++ myPose.py
@@ -2,104 +2,98 @@ import mediapipe as mp
 import cv2
 import math
 
+class myPose:
+    # Định nghĩa các hằng số ngưỡng
+    CLAP_THRESHOLD = 50
+    JUMP_THRESHOLD = 10
+    DOWN_THRESHOLD = 50
 
-class myPose():
     def __init__(self):
         self.mp_pose = mp.solutions.pose
         self.pose = self.mp_pose.Pose()
         self.mp_drawing = mp.solutions.drawing_utils
-        self.shoudler_line_y = 0  # Luu lai cai vi tri 2 vai cua nguoi dung khi vo tay bat dau game
+        self.shoulder_line_y = 0  # Lưu vị trí đường ngang của vai khi vỗ tay bắt đầu game
 
     def detectPose(self, image):
-        # Chuyển RGB
+        # Chuyển ảnh sang RGB để xử lý
         imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-        # Lay ket qua dau ra qua model
         results = self.pose.process(imageRGB)
 
         if results.pose_landmarks:
-            self.mp_drawing.draw_landmarks(image, landmark_list=results.pose_landmarks,
-                                           connections=self.mp_pose.POSE_CONNECTIONS,
-                                           landmark_drawing_spec=self.mp_drawing.DrawingSpec(color=(255, 225, 255),
-                                                                                             thickness=3,
-                                                                                             circle_radius=3),
-                                           connection_drawing_spec=self.mp_drawing.DrawingSpec(color=(0, 0, 255),
-                                                                                               thickness=2))
-
+            self.mp_drawing.draw_landmarks(
+                image, landmark_list=results.pose_landmarks,
+                connections=self.mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=self.mp_drawing.DrawingSpec(color=(255, 225, 255),
+                                                                   thickness=3,
+                                                                   circle_radius=3),
+                connection_drawing_spec=self.mp_drawing.DrawingSpec(color=(0, 0, 255),
+                                                                     thickness=2)
+            )
         return image, results
 
+    def _get_image_coords(self, landmark, image_shape):
+        image_height, image_width, _ = image_shape
+        return int(landmark.x * image_width), int(landmark.y * image_height)
+
     def checkPose_LRC(self, image, results):
-
-        # Lay kich thuoc anh dau vao
         image_height, image_width, _ = image.shape
-        image_mid_width = image_width // 2
+        mid_width = image_width // 2
 
-        leftShoulder_x = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER].x * image_width)
-        rightShoulder_x = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER].x * image_width)
+        leftShoulder = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER]
+        rightShoulder = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        leftShoulder_x, _ = self._get_image_coords(leftShoulder, image.shape)
+        rightShoulder_x, _ = self._get_image_coords(rightShoulder, image.shape)
 
-        if (leftShoulder_x < image_mid_width) and (rightShoulder_x < image_mid_width):
+        if leftShoulder_x < mid_width and rightShoulder_x < mid_width:
             LRC = "L"
-        elif (leftShoulder_x > image_mid_width) and (rightShoulder_x > image_mid_width):
+        elif leftShoulder_x > mid_width and rightShoulder_x > mid_width:
             LRC = "R"
         else:
             LRC = "C"
 
         cv2.putText(image, LRC, (5, image_height - 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
-        cv2.line(image, (image_mid_width, 0), (image_mid_width, image_height), (255, 255, 255), 2)
-
+        cv2.line(image, (mid_width, 0), (mid_width, image_height), (255, 255, 255), 2)
         return image, LRC
 
     def checkPose_JSD(self, image, results):
         image_height, image_width, _ = image.shape
 
-        leftShoulder_y = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER].y * image_height)
-        rightShoulder_y = int(
-            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER].y * image_height)
+        leftShoulder = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER]
+        rightShoulder = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        _, leftShoulder_y = self._get_image_coords(leftShoulder, image.shape)
+        _, rightShoulder_y = self._get_image_coords(rightShoulder, image.shape)
+        centerShoulder_y = (leftShoulder_y + rightShoulder_y) // 2
 
-        centerShoulder_y = abs(leftShoulder_y + rightShoulder_y) // 2
-
-        jump_threshold = 10
-        down_threshold = 50
-        if (centerShoulder_y < self.shoudler_line_y - jump_threshold):
+        if centerShoulder_y < self.shoulder_line_y - self.JUMP_THRESHOLD:
             JSD = "J"
-        elif (centerShoulder_y > self.shoudler_line_y + down_threshold):
+        elif centerShoulder_y > self.shoulder_line_y + self.DOWN_THRESHOLD:
             JSD = "D"
         else:
             JSD = "S"
 
         cv2.putText(image, JSD, (5, image_height - 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 3)
-        cv2.line(image, (0, self.shoudler_line_y), (image_width, self.shoudler_line_y), (0, 255, 255), 2)
-
+        cv2.line(image, (0, self.shoulder_line_y), (image_width, self.shoulder_line_y), (0, 255, 255), 2)
         return image, JSD
 
     def checkPose_Clap(self, image, results):
-
         image_height, image_width, _ = image.shape
 
-        left_hand = (results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST].x * image_width,
-                     results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST].y * image_height)
+        left_hand = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST]
+        right_hand = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST]
+        left_coords = self._get_image_coords(left_hand, image.shape)
+        right_coords = self._get_image_coords(right_hand, image.shape)
 
-        right_hand = (results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST].x * image_width,
-                      results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST].y * image_height)
+        distance = int(math.hypot(left_coords[0] - right_coords[0], left_coords[1] - right_coords[1]))
 
-        distance = int(math.hypot(left_hand[0] - right_hand[0], left_hand[1] - right_hand[1]))
-
-        clap_threshold = 100
-        if distance < clap_threshold:
-            CLAP = "C"
-        else:
-            CLAP = "N"
-
+        CLAP = "C" if distance < self.CLAP_THRESHOLD else "N"
         cv2.putText(image, CLAP, (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 0), 3)
-
         return image, CLAP
 
     def save_shoulder_line_y(self, image, results):
-        image_height, image_width, _ = image.shape
+        image_height, _, _ = image.shape
 
-        leftShoulder_y = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER].y * image_height)
-        rightShoulder_y = int(
-            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER].y * image_height)
-
-        self.shoudler_line_y = abs(leftShoulder_y + rightShoulder_y) // 2
-        return
+        leftShoulder = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER]
+        rightShoulder = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER]
+        _, left_y = self._get_image_coords(leftShoulder, image.shape)
+        _, right_y = self._get_image_coords(rightShoulder, image.shape)
+        self.shoulder_line_y = (left_y + right_y) // 2
--- test.py
+++ test.py
@@ -2,99 +2,94 @@ import cv2
 import pyautogui
 from myPose import myPose
 
-class myGame():
+class MyGame:
+    # Định nghĩa các hằng số vị trí
+    LEFT, CENTER, RIGHT = 0, 1, 2
+    DOWN, STAND, JUMP = 0, 1, 2
+    CLAP_THRESHOLD = 10  # Số frame cần để kích hoạt hành động từ vỗ tay
+
     def __init__(self):
         self.pose = myPose()
         self.game_started = False
-        self.x_position = 1 # 0: Ray ben trai, 1: Ray giua, 2: Ray ben phai
-        self.y_position = 1 # 0: Down, 1: Stand, 2: jump
-        self.clap_duration = 0 # So frame ma ng dung vo tay
+        self.x_position = self.CENTER
+        self.y_position = self.STAND
+        self.clap_duration = 0
 
-    def move_LRC(self, LRC):
-        if LRC=="L":
-            if self.x_position == 1:
-                pyautogui.press('left')
-                self.x_position = 0
-            elif self.x_position == 2:
-                pyautogui.press('left')
-                self.x_position = 1
-        elif LRC=="R":
-            if self.x_position ==1:
-                pyautogui.press('right')
-                self.x_position = 2
-            elif self.x_position == 0:
-                pyautogui.press('right')
-                self.x_position = 1
-        elif LRC=="C":
-            if self.x_position == 0:
-                pyautogui.press('right')
-            elif self.x_position == 2:
-                pyautogui.press('left')
-            self.x_position = 1
-        return
+    def move_LRC(self, direction):
+        """Di chuyển sang trái, phải hoặc trở về giữa."""
+        if direction == "L" and self.x_position > self.LEFT:
+            pyautogui.press('left')
+            self.x_position -= 1
+        elif direction == "R" and self.x_position < self.RIGHT:
+            pyautogui.press('right')
+            self.x_position += 1
+        elif direction == "C" and self.x_position != self.CENTER:
+            # Chuyển về giữa: nếu đang ở bên trái thì bấm phải, nếu ở bên phải thì bấm trái
+            pyautogui.press('right' if self.x_position == self.LEFT else 'left')
+            self.x_position = self.CENTER
 
-    def move_JSD(self, JSD):
-        if (JSD=="J") and (self.y_position == 1):
+    def move_JSD(self, action):
+        """Điều khiển hành động nhảy, cúi xuống hoặc trở về trạng thái đứng."""
+        if action == "J" and self.y_position == self.STAND:
             pyautogui.press('up')
-            self.y_position = 2
-        elif (JSD=="D") and (self.y_position ==1):
+            self.y_position = self.JUMP
+        elif action == "D" and self.y_position == self.STAND:
             pyautogui.press('down')
-            self.y_position = 0
-        elif (JSD=="S") and (self.y_position !=1):
-            self.y_position = 1
-        return
+            self.y_position = self.DOWN
+        elif action == "S" and self.y_position != self.STAND:
+            self.y_position = self.STAND
+
+    def reset_game(self, image, results):
+        """Reset game khi nhận đủ số frame vỗ tay."""
+        # Nếu game đã bắt đầu thì reset trạng thái, ngược lại bắt đầu game mới
+        if self.game_started:
+            self.x_position = self.CENTER
+            self.y_position = self.STAND
+            pyautogui.press('enter')
+        else:
+            self.game_started = True
+            # Có thể kích hoạt click chuột nếu cần
+            # pyautogui.click(x=720, y=560, button="left")
+        self.pose.save_shoulder_line_y(image, results)
+        self.clap_duration = 0
 
     def play(self):
-
-        # Khoi tao camera
+        """Vòng lặp chính của game."""
         cap = cv2.VideoCapture(0)
-        cap.set(3, 1280)
-        cap.set(4, 960)
 
         while True:
             ret, image = cap.read()
-            if ret:
+            if not ret:
+                continue
 
-                image = cv2.flip(image, 1)
-                image_height, image_width, _ = image.shape
-                image, results = self.pose.detectPose(image)
+            image = cv2.flip(image, 1)
+            image_height, image_width, _ = image.shape
+            image, results = self.pose.detectPose(image)
 
-                if results.pose_landmarks:
-                    # Kiem tra game da bat dau chua
-                    if self.game_started:
-                        # Kiem tra trai phai
-                        image, LRC = self.pose.checkPose_LRC(image, results)
-                        self.move_LRC(LRC)
+            if results.pose_landmarks:
+                if self.game_started:
+                    # Kiểm tra chuyển động trái/giữa/phải
+                    image, LRC = self.pose.checkPose_LRC(image, results)
+                    self.move_LRC(LRC)
 
-                        # Kiem tra len xuong
-                        image, JSD = self.pose.checkPose_JSD(image, results)
-                        self.move_JSD(JSD)
-                    else:
-                        cv2.putText(image, "Clap your hand to start!", (5, image_height-10), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,0), 3)
+                    # Kiểm tra hành động nhảy/cúi/đứng
+                    image, JSD = self.pose.checkPose_JSD(image, results)
+                    self.move_JSD(JSD)
+                else:
+                    cv2.putText(image, "Clap your hand to start!",
+                                (5, image_height - 10),
+                                cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 0), 3)
 
-                    image, CLAP = self.pose.checkPose_Clap(image, results)
-                    if CLAP == "C":
-                        self.clap_duration +=1
+                # Kiểm tra hành động vỗ tay
+                image, CLAP = self.pose.checkPose_Clap(image, results)
+                if CLAP == "C":
+                    self.clap_duration += 1
+                    if self.clap_duration == self.CLAP_THRESHOLD:
+                        self.reset_game(image, results)
+                else:
+                    self.clap_duration = 0
 
-                        if self.clap_duration == 10: #10 frame
-                            if self.game_started:
-                                # Reset
-                                self.x_position  = 1
-                                self.y_position  = 1
-                                self.pose.save_shoulder_line_y(image, results)
-                                pyautogui.press('space')
-                            else:
-                                self.game_started  = True
-                                self.pose.save_shoulder_line_y(image, results)
-                                pyautogui.click(x=720, y = 560, button = "left")
-
-                            self.clap_duration = 0
-                    else:
-                        self.clap_duration = 0
-
-
-                cv2.imshow("Game", image)
-
+            cv2.imshow("Game", image)
             if cv2.waitKey(1) == ord('q'):
                 break
 
@@ -102,5 +97,5 @@ class myGame():
         cv2.destroyAllWindows()
 
 
-myGame = myGame()
-myGame.play()
+if __name__ == "__main__":
+    MyGame().play()
